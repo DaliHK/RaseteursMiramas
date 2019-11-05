5e8dbbea3d4500ddb0c1f9a2be5b63d82594d40b
bryan_nativel <bryan.nativel@hotmail.fr>
2019-11-05 14:05:21 +0100

    Avant le pull de la branch login

--- src/Controller/WebsiteController.php
+++ src/Controller/WebsiteController.php
@@ -2,20 +2,51 @@
 
 namespace App\Controller;
 
-use App\Repository\AdherentRepository;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class WebsiteController extends AbstractController
+
 {
     /**
-     * @Route("/adherent", name="page_adherent")
+     * @Route("adherent/profile", name="adherent_profile")
+     * @param UserInterface
+     */
+    public function adherentProfile(UserInterface $user){
+
+
+        return $this -> render('/website/adherentProfile.html.twig',[
+            'user'=> $user
+        ]);
+
+
+    }
+
+
+    /**
+     * @Route("adherent/profile/edit", name="adherent_edit_profile")
+     * @param Request $request
+     * @param UserInterface $user
+     * @return RedirectResponse|Response
      */
     
-    public function index()
-
+    public function adherentEditProfile(Request $request, UserInterface $user)
     {
-        return $this->render('website/index.html.twig');
-         
+        //affiche le formulaire deja enregistré de l'user pour qu'il puisse le consulter ou modifier
+        $form = $this->createForm(AdherentFormType::class, $user);
+        //j'envoie les informations modifié à la base de données 
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+            return $this->redirectToRoute('adherent_profile');
+        }
+        return $this->render('/website/adherentEditProfile.html.twig', [
+            'user' => $user,
+            'form' => $form->createView()
+        ]);
     }
+
 }
