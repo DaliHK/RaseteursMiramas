4244343d24dd1584eaa367ffc16eaac14d035e32
Frayel <frazier.yeliz@protonmail.com>
2019-11-06 15:43:10 +0100

    ajout gestion erreur

--- src/Controller/AdherentController.php
+++ src/Controller/AdherentController.php
@@ -7,10 +7,13 @@ use App\Form\RegistrationType;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\User\User;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Csrf\TokenGenerator\TokenGeneratorInterface;
 
 
 class AdherentController extends AbstractController
