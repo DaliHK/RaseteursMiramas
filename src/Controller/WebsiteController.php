33f83cf2c42ed2e95b4c7947becc1457a30178ab
bryan_nativel <bryan.nativel@hotmail.fr>
2019-11-06 09:38:00 +0100

    Probléme lorsque il y a modification du profile adherent

--- src/Controller/WebsiteController.php
+++ src/Controller/WebsiteController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Form\EditAdherentType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -10,19 +12,27 @@ class WebsiteController extends AbstractController
 
 {
     /**
-     * @Route("adherent/profile", name="adherent_profile")
-     * @param UserInterface
+     * @Route("/", name="home")
      */
-    public function adherentProfile(UserInterface $user){
 
-
-        return $this -> render('/website/adherentProfile.html.twig',[
-            'user'=> $user
-        ]);
-
+    public function home(){
+    
+        return $this -> render('/home/home.html.twig');
 
     }
 
+    /**
+     * @Route("adherent/profile", name="adherent_profile")
+     * @param UserInterface $userProfile
+     */
+
+    public function adherentProfile(UserInterface $userProfile){
+
+
+        return $this -> render('/website/adherentProfile.html.twig',[
+            'user'=> $userProfile
+        ]);
+    }
 
     /**
      * @Route("adherent/profile/edit", name="adherent_edit_profile")
@@ -31,21 +41,22 @@ class WebsiteController extends AbstractController
      * @return RedirectResponse|Response
      */
     
-    public function adherentEditProfile(Request $request, UserInterface $user)
+    public function adherentEditProfile(Request $request, UserInterface $userProfile)
     {
         //affiche le formulaire deja enregistré de l'user pour qu'il puisse le consulter ou modifier
-        $form = $this->createForm(AdherentFormType::class, $user);
+        $form = $this->createForm(EditAdherentType::class, $userProfile);
         //j'envoie les informations modifié à la base de données 
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
+            dump($userProfile);
             $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($user);
+            $entityManager->persist($userProfile);
             $entityManager->flush();
-            return $this->redirectToRoute('adherent_profile');
+            return $this->redirectToRoute("{{path('adherent_profile')}}");
         }
         return $this->render('/website/adherentEditProfile.html.twig', [
-            'user' => $user,
-            'form' => $form->createView()
+            'user' => $userProfile,
+            'adherent' => $form->createView()
         ]);
     }
 
---
+++ src/Form/EditAdherentType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Adherent;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+
+class EditAdherentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            /* ->add('username') */
+            /* ->add('roles') */
+            ->add('password',PasswordType::class)
+            ->add('nom')
+            ->add('prenom')
+           /*  ->add('dateNaissance')
+            ->add('dateInscription') */
+            ->add('email',EmailType::class)
+            ->add('telephone',NumberType::class)
+            ->add('adresse')
+            ->add('cp')
+            ->add('ville')
+            ->add('numeroUrgence',NumberType::class)
+            /* ->add('statut')
+            ->add('cotisationAsso')
+            ->add('cotisationLicence')
+            ->add('numLicence')
+            ->add('dossierInscription')
+            ->add('participation') */
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Adherent::class,
+        ]);
+    }
+}
