f49a6eb5111a8a71628473cb9c2b38e8199ae738
bryan_nativel <bryan.nativel@hotmail.fr>
2019-11-07 11:40:03 +0100

    Avant l'ajout de la table intermediére entre adherent et evenement

--- src/Controller/WebsiteController.php
+++ src/Controller/WebsiteController.php
@@ -2,11 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Evenement;
 use App\Form\EditAdherentType;
+use App\Repository\AdherentRepository;
+use App\Repository\EvenementRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class WebsiteController extends AbstractController
 
@@ -26,11 +30,14 @@ class WebsiteController extends AbstractController
      * @param UserInterface $userProfile
      */
 
-    public function adherentProfile(UserInterface $userProfile){
+    public function adherentProfile(UserInterface $userProfile ,AdherentRepository $adherent){
 
-
+        dump($adherent);
+        die;  
         return $this -> render('/website/adherentProfile.html.twig',[
+            
             'user'=> $userProfile
+           
         ]);
     }
 
@@ -39,16 +46,23 @@ class WebsiteController extends AbstractController
      * @param Request $request
      * @param UserInterface $user
      * @return RedirectResponse|Response
+     * @param UserPasswordEncoderInterface $passwordEncoder
      */
     
-    public function adherentEditProfile(Request $request, UserInterface $userProfile)
+    public function adherentEditProfile(Request $request, UserInterface $userProfile, UserPasswordEncoderInterface $encoder)
     {
+        
+
         //affiche le formulaire deja enregistré de l'user pour qu'il puisse le consulter ou modifier
         $form = $this->createForm(EditAdherentType::class,$userProfile);
         //j'envoie les informations modifié à la base de données 
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             
+            // Pour encoder le password quand il est modifié
+            $hash = $encoder->encodePassword($userProfile, $userProfile->getPassword()); 
+            $userProfile->setPassword($hash);  
+
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($userProfile);
             $entityManager->flush();
@@ -59,6 +73,24 @@ class WebsiteController extends AbstractController
             'user' => $userProfile,
             'adherent' => $form->createView()
         ]);
+    }
+
+
+    /**
+     * @Route("adherent/profile/{id}", name="supression_evenement_inscrit")
+     */
+
+    public function FunctionName(UserInterface $userProfile)
+    {
+     
+        $id = $userProfile->getEvenement()->getOwner();
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->remove($id); 
+        $entityManager->flush();
+
+        return $this->render('adherent_profile');
+
+           
     }
 
 }
