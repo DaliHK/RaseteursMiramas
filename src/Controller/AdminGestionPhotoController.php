7044ca2799f3b72f103137bc1c94364e67580ea6
bryan_nativel <bryan.nativel@hotmail.fr>
2019-11-19 12:42:50 +0100

    Supression entity SourcePhoto

--- src/Controller/AdminGestionPhotoController.php
+++ src/Controller/AdminGestionPhotoController.php
@@ -2,73 +2,85 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+use App\Form\SourcePhotoType;
+use App\Repository\SourcePhotoRepository;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdminGestionPhotoController extends AbstractController
 {
 
     /**
      * @Route("/admin/gestion/photo", name="admin_gestion_photo")
+     * @param Request $request
+     * @param Filesystem $fileNames
+     * @param SourcePhotoRepository $sourcePhoto
      */
 
-    public function gestionPhoto()
+    public function gestionPhoto(Request $request,Filesystem $filesystem, SourcePhotoRepository $sourcePhoto)
     {
-         //Instencie la variable NewPicture pour utiliser c'est prop  et affecte la variable carouselForm pour crée le form.
-        $newPicture = new Carousel();
-        $carouselForm = $this->createForm(AdminCarouselType::class, $newPicture);
-        $carouselForm->handleRequest($request);
+         //Instencie la variable NewPicture pour utiliser c'est prop  et affecte la variable sourcePhotoForm pour crée le form.
+        $newPicture = new SourcePhoto();
+        $sourcePictureForm = $this->createForm(SourcePhotoType::class, $newPicture);
+        $sourcePictureForm->handleRequest($request);
        
-        if ($carouselForm->isSubmitted() && $carouselForm->isValid()) {
-
+        if ($sourcePictureForm->isSubmitted() && $sourcePictureForm->isValid()) {
 
             // Stock les fichiés  uploader dans une variable
-            $picture1 = $newPicture->getPhoto1();
-            $picture2 = $newPicture->getPhoto2();
-            $picture3 = $newPicture->getPhoto3();
-            $picture4 = $newPicture->getPhoto4();
-
-            $path = $this->getParameter('carousel_directory');
+            $category = $newPicture->getCategorie();
+            $title = $newPicture->getTitre();
+            $date = $newPicture->getDate();
+           
+            $path = $this->getParameter('sourcePhoto_directory');
 
             //Crée un dossier avec l'id de l'adherent connecté à l'amplacement du $path
              if (!$path) {
 
-                $path = $filesystem->mkdir($path.'carousel',0700);
+                $path = $filesystem->mkdir($path.'source_photo',0700);
+                
             }
 
             // Géneration de nom unique pour les fichiers pour éviter les doublons et sécuriser
-            $arrayPicture=[$picture1,$picture2,$picture3,$picture4];
-            $fileNames = [];
+           /*  $arrayPicture=[$picture1,$picture2,$picture3,$picture4];
+            $fileNames = []; */
 
-            for ($i=0; $i <count($arrayPicture) ; $i++) { 
+           /*  for ($i=0; $i <count($arrayPicture) ; $i++) { 
 
                $fileNames[] = $this->generateUniqueFileName().'.'.$arrayPicture[$i]->guessExtension();
             }
-
+            */
+            $originalFilename = pathinfo($title->getClientOriginalName(), PATHINFO_FILENAME);
+            $fileNames = $this->generateUniqueFileName().'/'.$originalFilename.'.'.$title->guessExtension();
+ 
             // Envoie les fichiés dans le dossier carousel
-            for ($a=0; $a <count($fileNames) ; $a++) { 
+           /*  for ($a=0; $a <count($fileNames) ; $a++) { 
               $arrayPicture[$a]->move($path , 
                     $fileNames[$a]);
             }
+            */
 
+            $title->move($path , 
+                    $fileNames);
             
             //Envoie les noms relié au fichier dans la BDD
-            $newPicture->setphoto1($fileNames[0]);
-            $newPicture->setphoto2($fileNames[1]);
-            $newPicture->setphoto3($fileNames[2]);
-            $newPicture->setphoto4($fileNames[3]);
-            
+            $newPicture->setphoto1($fileNames);
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($newPicture);
             $entityManager->flush();
 
 
-            return $this->redirect($this->generateUrl('admin_carousel'));
+            return $this->redirect($this->generateUrl('admin_gestion_photo'));
 
         }
 
         return $this->render('admin_gestion_photo/gestionPhoto.html.twig', [
-            'controller_name' => 'AdminGestionPhotoController',
+
+             'sourcePictureForm' => $sourcePictureForm->createView(),
+             'sourcePhoto' => $sourcePhoto->findAll()
+
         ]);
     }
 }
